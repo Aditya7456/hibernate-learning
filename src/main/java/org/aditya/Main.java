bbadda28c871ee841abe32421665fe8801af719d
Aditya <adityaprajapati139@gmail.com>
2026-01-30 14:19:25 +0530

    added fetch data feature

--- src/main/java/org/aditya/Main.java
+++ src/main/java/org/aditya/Main.java
@@ -11,25 +11,28 @@ public class Main {
         s1.setName("Neelam");
         s1.setRollNo(125);
         s1.setAge(36);
+        Student s2 = null;
 
         // first need to create configuration object
-        Configuration cfg = new Configuration();
-        cfg.addAnnotatedClass(Student.class);
-        cfg.configure();
-
-
+        //Configuration cfg = new Configuration();
+        //cfg.addAnnotatedClass(Student.class);
+        //cfg.configure();
         // now need a object of SessionFactory
-        SessionFactory sf = cfg.buildSessionFactory();
-
+        //SessionFactory sf = cfg.buildSessionFactory();
+        SessionFactory factory = new Configuration()
+                            .addAnnotatedClass(Student.class)
+                            .configure()
+                            .buildSessionFactory();
         // now need session object
-        Session session = sf.openSession();
-
+        Session session = factory.openSession();
         // create the object of Transaction
-        Transaction transaction = session.beginTransaction();
+        //Transaction transaction = session.beginTransaction();
+        s2 = session.find(Student.class,103);
 
-        session.persist(s1);
-        transaction.commit();
+        //session.persist(s1); // to persist the data in the database
+        //transaction.commit();
+        factory.close();
 
-        System.out.println(s1);
+        System.out.println(s2);
     }
 }
