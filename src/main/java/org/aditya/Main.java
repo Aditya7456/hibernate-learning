8d8df9756e4688a6bb96e01de3a034c5687bf342
Aditya <adityaprajapati139@gmail.com>
2026-02-04 20:17:42 +0530

    changed some configuration

--- src/main/java/org/aditya/Main.java
+++ src/main/java/org/aditya/Main.java
@@ -9,88 +9,16 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        //Student s1 = null;
-//        Student s1 = new Student();
-//        s1.setName("Poonam");
-//        s1.setRollNo(201);
-//        s1.setAge(46);
-        //Student s2 = null;
-
-        /* persist()  -> to save the data
-           find()   -> to find the data
-           merge()  -> to update and save both
-           remove()  -> to remove the data from the database
-         */
-
-
-
-        // first need to create configuration object
-        //Configuration cfg = new Configuration();
-        //cfg.addAnnotatedClass(Student.class);
-        //cfg.configure();
-        // now need a object of SessionFactory
-        //SessionFactory sf = cfg.buildSessionFactory();
-        Laptop l1 = new Laptop();
-        l1.setBrand("hp");
-        l1.setModel("probook");
-        l1.setRam(16);
-
-        Laptop l2 = new Laptop();
-        l2.setBrand("Asus");
-        l2.setModel("Rog");
-        l2.setRam(16);
-
-        Laptop l3 = new Laptop();
-        l3.setBrand("Apple");
-        l3.setModel("Macbook");
-        l3.setRam(8);
-
-
-        Alien a1 = new Alien();
-        a1.setAname("Rahul");
-        a1.setTech("Java");
-
-        Alien a2 = new Alien();
-        a2.setAname("Harsh");
-        a2.setTech("Python");
-
-        a1.setLaptops(Arrays.asList(l1,l2));
-        a2.setLaptops(Arrays.asList(l3));
 
         SessionFactory factory = new Configuration()
-                            .addAnnotatedClass(Alien.class)
                             .addAnnotatedClass(Laptop.class)
                             .configure()
                             .buildSessionFactory();
         // now need session object
         Session session = factory.openSession();
-
-        //s1 = session.find(Student.class,119);
-        // create the object of Transaction
-        Transaction transaction = session.beginTransaction();
-        //s2 = session.find(Student.class,103);
-        //session.merge(s1); it will update and save based on if data is there or not
-        // to update the data save() is depricated.
-        // now deleting the studen from the database;
-        //session.remove(s1);
-        session.persist(l1);
-        session.persist(l2);
-        session.persist(l3);
-
-        session.persist(a1);
-        session.persist(a2);
-
-
-        //session.persist(s1); // to persist the data in the database
-        transaction.commit();
-
-
+        Laptop l1 = session.find(Laptop.class,3);
+        System.out.println(l1);
         session.close();
-
-        Session session1 = factory.openSession();
-        Alien a5  = session1.find(Alien.class, 1 );
-        //System.out.println(a5);
-        session1.close();
         factory.close();
 
 
