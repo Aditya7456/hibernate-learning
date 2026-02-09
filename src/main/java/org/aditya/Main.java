bc45cf5eb0b9f03d27afa3de304c63f4263ea64c
Aditya <adityaprajapati139@gmail.com>
2026-02-09 19:51:05 +0530

    implemented eager and lazy loading

--- src/main/java/org/aditya/Main.java
+++ src/main/java/org/aditya/Main.java
@@ -2,12 +2,8 @@ package org.aditya;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
-import org.hibernate.query.Query;
 
-import java.util.Arrays;
-import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
@@ -16,30 +12,12 @@ public class Main {
                             .addAnnotatedClass(Laptop.class)
                             .configure()
                             .buildSessionFactory();
-        // now need session object
         Session session = factory.openSession();
-        // in sql use tableName
-        // if we sql then we have to write "select * from laptop where ram=32";
-        // if hql then "from Laptop where ram=32"; // use className
-        String brand = "Asus";
-        int ram = 32    ;
-        Query query = session.createQuery("select brand,model from Laptop where brand like ?1");
-        query.setParameter(1, brand);
-        //query.setParameter(2, ram);
-        List<Object[]> laptops = query.getResultList();
-
-        // Laptop l1 = session.find(Laptop.class,3);
-        //System.out.println(laptops);
-        for(Object []data: laptops){
-            System.out.println(data[0] + " "  + data[1]);
-        }
+        //Laptop laptop = session.find(Laptop.class, 2);
+        //session.load(Laptop.class,2);
+        Laptop laptop = session.getReference(Laptop.class,1);
+        System.out.println(laptop);
         session.close();
         factory.close();
     }
 }
-
-/*
-   Transaction transaction = session.beginTransaction();
-   we need transaction object, if we are doing any changes in database.
-   if we are just reading the data then transaction object is just optional.
- */
